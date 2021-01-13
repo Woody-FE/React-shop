37f39639902886af5c3f9ec0a202b9cdcd1a90d1
Taein Kim <kti91@likelion.org>
2021-01-13 23:19:40 +0900

    210113 | studying scss & styled-components

--- src/Detail.js
+++ src/Detail.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useHistory, useParams } from 'react-router-dom';
+import styled from 'styled-components';
+import './Detail.scss';
+
+let Box = styled.div`
+	padding-top: 30px;
+`;
+let Title = styled.h4`
+	font-size: 25px;
+	color: ${(props) => props.color};
+`;
 
 export default function Detail(props) {
+	let [open, setOpen] = useState(true);
+	useEffect(() => {
+		let Timer = setTimeout(() => {
+			setOpen(false);
+		}, 2000);
+		// 언마운트시에 타임아웃을 제거해줘야 다른 문제가 발생하지 않음
+		return () => {
+			clearTimeout(Timer);
+		};
+	}, []);
+
 	let { id } = useParams();
 	let history = useHistory();
 	let shoe = props.shoes.find((shoe) => {
@@ -9,6 +30,17 @@ export default function Detail(props) {
 	});
 	return (
 		<div className='container'>
+			<Box>
+				<Title className='red' color='blue'>
+					Detail
+				</Title>
+			</Box>
+			{open ? (
+				<div className='my-alert'>
+					<p>재고가 얼마 남지 않았습니다</p>
+				</div>
+			) : null}
+
 			<div className='row'>
 				<div className='col-md-6'>
 					<img
